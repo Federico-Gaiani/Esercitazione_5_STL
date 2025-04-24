74746c175f2b0ae98e8f3da6651c15dbeca4366a
Federico-Gaiani <s309900@studenti.polito.it>
2025-04-24 11:06:42 +0200

    cell0D e 1D

--- main.cpp
+++ main.cpp
@@ -1,4 +1,86 @@
+#include <iostream>
+#include "PolygonalMesh.hpp"
+#include "Utils.hpp"
+#include "UCDUtilities.hpp"
+
+using namespace std;
+using namespace Eigen;
+using namespace PolygonalLibrary;
+
+
 int main()
 {
+	PolygonalMesh mesh;
+
+    if(!ImportMesh(mesh))
+    {
+        cerr << "file not found" << endl;
+        return 1;
+    }
+/*	
+	for (unsigned int i=0;i<mesh.NumCell0Ds;i++){
+		cout<<mesh.Cell0DsId[i]<<" "<<mesh.Cell0DsCoordinates (0,i)<<" "<<mesh.Cell0DsCoordinates(1,i)<<endl;
+	}
+
+	for (auto& it : mesh.MarkerCell0Ds[5])
+		cout<<it<<" ";
+	cout<<endl;
+
+	for (unsigned int i=0;i<mesh.NumCell1Ds;i++){
+		cout<<mesh.Cell1DsId[i]<<" "<<mesh.Cell1DsExtrema (0,i)<<" "<<mesh.Cell1DsExtrema(1,i)<<endl;
+	}
+
+	for (auto& it : mesh.MarkerCell1Ds[5])
+		cout<<it<<" ";
+	cout<<endl;
+*/
+
+
+	Gedim::UCDUtilities utilities;
+    {
+        vector<Gedim::UCDProperty<double>> cell0Ds_properties(1);
+
+        cell0Ds_properties[0].Label = "Marker";
+        cell0Ds_properties[0].UnitLabel = "-";
+        cell0Ds_properties[0].NumComponents = 1;
+
+        vector<double> cell0Ds_marker(mesh.NumCell0Ds, 0.0);
+        for(const auto &m : mesh.MarkerCell0Ds)
+            for(const unsigned int id: m.second)
+                cell0Ds_marker.at(id) = m.first;
+
+        cell0Ds_properties[0].Data = cell0Ds_marker.data();
+
+        utilities.ExportPoints("./Cell0Ds.inp",
+                               mesh.Cell0DsCoordinates,
+                               cell0Ds_properties);
+    }
+
+    {
+
+        vector<Gedim::UCDProperty<double>> cell1Ds_properties(1);
+
+        cell1Ds_properties[0].Label = "Marker";
+        cell1Ds_properties[0].UnitLabel = "-";
+        cell1Ds_properties[0].NumComponents = 1;
+
+        vector<double> cell1Ds_marker(mesh.NumCell1Ds, 0.0);
+        for(const auto &m : mesh.MarkerCell1Ds)
+            for(const unsigned int id: m.second)
+                cell1Ds_marker.at(id) = m.first;
+
+        cell1Ds_properties[0].Data = cell1Ds_marker.data();
+
+        utilities.ExportSegments("./Cell1Ds.inp",
+                                 mesh.Cell0DsCoordinates,
+                                 mesh.Cell1DsExtrema,
+                                 {},
+                                 cell1Ds_properties);
+    }
+	
+	
+	
+	
+	
     return 0;
 }
